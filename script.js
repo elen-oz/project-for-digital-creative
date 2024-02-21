a34d6f13e9e6351053ab4193758a2184c4871f34
elen-oz <ozerova.lenka@gmail.com>
2024-02-21 20:41:47 +0100

    vision section is ready

---
+++ script.js
@@ -0,0 +1,6 @@
+function changepage() {
+    let elem = document.getElementById("header1");
+    elem.style["background-color"] = "red";
+    console.log(elem.innerHTML);
+    elem.style["fontFamily"] = "monospace";
+  }
