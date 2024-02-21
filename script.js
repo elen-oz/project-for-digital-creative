a43ff2775007598dec436484ed5a365b9f97245b
elen-oz <ozerova.lenka@gmail.com>
2024-02-21 22:00:31 +0100

    remove unnes files

--- script.js
+++
@@ -1,6 +0,0 @@
-function changepage() {
-    let elem = document.getElementById("header1");
-    elem.style["background-color"] = "red";
-    console.log(elem.innerHTML);
-    elem.style["fontFamily"] = "monospace";
-  }
